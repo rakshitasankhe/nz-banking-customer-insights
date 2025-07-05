ef6c39b99ad707b20a24a637eb0a75de92811616
Rakshita Sankhe <rakshita.sankhe@gmail.com>
2025-07-05 15:33:39 +1200

    updt

--- app.py
+++ app.py
@@ -30,4 +30,4 @@ age = st.sidebar.slider("Age", 18, 90, 35)
 tenure = st.sidebar.slider("Tenure (Years)", 0, 10, 3)
 account_balance = st.sidebar.number_input("Account Balance ($)", min_value=0.0, max_value=100000.0, value=5000.0, step=100.0)
 is_active = st.sidebar.selectbox("Is Account Active?", options=[1, 0], format_func=lambda x: "Yes" if x==1 else "No")
-num_products = st.sidebar.slider("Number of Products", 1, 5, 1)_
+num_products = st.sidebar.slider("Number of Products", 1, 5, 1)
